aab38882674f70eb8d60bcea4b34eb965c0e8d2b
Bair Arshaev <ars-bair@yandex.ru>
2022-05-30 23:37:44 +0300

    Edit code 2

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -3,19 +3,18 @@ import org.junit.jupiter.api.Test;
 
 public class CalcTest {
 
-    private final double EPS = 1e-9;
-    private final double PI = 3.14;
-    private final int ARGUMENT = 1;
+    private static final double EPS = 1e-9;
+    private static final int ARGUMENT = 15;
+    private static final double RESULT_CIRCLE_AREA = 3.14 * ARGUMENT * ARGUMENT;
+    private static final double RESULT_CIRCLE_LENGTH = 2 * 3.14 * ARGUMENT;
 
     @Test
     public void circleAreaTest() {
-        Assertions.assertEquals(PI, Calc.circleArea(ARGUMENT), EPS);
+        Assertions.assertEquals(RESULT_CIRCLE_AREA, Calc.circleArea(ARGUMENT), EPS);
     }
 
     @Test
     public void circleLengthTest() {
-        Assertions.assertEquals(2 * PI, Calc.circleLength(ARGUMENT), EPS);
+        Assertions.assertEquals(RESULT_CIRCLE_LENGTH, Calc.circleLength(ARGUMENT), EPS);
     }
-
-
 }
